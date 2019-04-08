9f900a42bfffdf1522cbd79960e6a95fcf0f6185
CalebMorin <cmorin2@my.centennialcollege.ca>
2019-04-08 16:06:18 -0400

    Added all methods except Color and Point

--- Assignment4_Comp123_Group001/Program.cs
+++ Assignment4_Comp123_Group001/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Assignment4_Comp123_Group001
 {
-    //Interfaces
+    //***Interfaces***
     interface IDrawable
     {
 
@@ -25,20 +25,52 @@ namespace Assignment4_Comp123_Group001
     //Child classes
     class DrawableRectangle : Primitive
     {
+        double Area()
+        {
 
+        }
+        void Draw()
+        {
+
+        }
+        DrawableRectangle();
+        void Write()
+        {
+
+        }
     }
     class DrawableEllipse : Primitive
     {
+        double Area()
+        {
 
+        }
+        void Draw()
+        {
+
+        }
+        DrawableEllipse();
     }
     class DrawableArc : Primitive
     {
+        //Fields
+        float end;
+        float start;
+        //Methods
+        void Draw()
+        {
 
+        }
+        DrawableArc();
+        void Write()
+        {
+
+        }
     }
     //(Han) Solo classes
     class DrawableBezier
     {
-
+        
     }
     class DrawableLine
     {
