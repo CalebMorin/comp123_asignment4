36191b1b29c18655ffefafb4a29c835530c0e8e5
CalebMorin <cmorin2@my.centennialcollege.ca>
2019-04-11 21:48:15 -0400

    added main() class content

--- Assignment4_Comp123_Group001/Program.cs
+++ Assignment4_Comp123_Group001/Program.cs
@@ -10,6 +10,60 @@ using System.Drawing.Text;
 
 namespace Assignment4_Comp123_Group001
 {
+    public void Main()
+    {
+        //interfaces are types.
+        List<IDrawable> face = new List<IDrawable>();
+
+        face.Add(new DrawableEllipse(Color.Yellow, true, new Rectangle(75, 160, 40, 70)));      //left ear
+        face.Add(new DrawableEllipse(Color.BlueViolet, false, new Rectangle(75, 160, 40, 70))); //left ear
+        face.Add(new DrawableEllipse(Color.Yellow, true, new Rectangle(285, 160, 40, 70)));     //right ear
+        face.Add(new DrawableEllipse(Color.BlueViolet, false, new Rectangle(285, 160, 40, 70)));//right ear
+        face.Add(new DrawableRectangle(Color.Salmon, true, new Rectangle(100, 100, 200, 300))); //face
+        face.Add(new DrawableRectangle(Color.White, true, new Rectangle(140, 165, 45, 60)));    //right eye
+        face.Add(new DrawableRectangle(Color.White, true, new Rectangle(220, 165, 45, 60)));    //left eye
+        face.Add(new DrawableRectangle(Color.Black, true, new Rectangle(150, 183, 25, 40)));    //right pupil
+        face.Add(new DrawableRectangle(Color.Black, true, new Rectangle(230, 183, 25, 40)));    //left pupil
+        face.Add(new DrawableRectangle(Color.Brown, true, new Rectangle(90, 10, 220, 120)));    //hat top
+        face.Add(new DrawableRectangle(Color.Brown, true, new Rectangle(10, 100, 380, 20)));    //hat rim
+
+        face.Add(new DrawableBezier(Color.Black, new Point(195, 240), new Point(135, 280), new Point(275, 280), new Point(215, 240)));
+
+        face.Add(new DrawableEllipse(Color.Red, false, new Rectangle(150, 300, 100, 35)));      //lips
+        face.Add(new DrawableEllipse(Color.Wheat, true, new Rectangle(160, 305, 80, 25)));      //mouth
+        face.Add(new DrawableLine(Color.Red, new Point(105, 30), new Point(105, 100)));         //lines
+        face.Add(new DrawableLine(Color.Orange, new Point(127, 30), new Point(127, 100)));      //lines
+        face.Add(new DrawableLine(Color.Yellow, new Point(155, 30), new Point(155, 100)));      //lines
+        face.Add(new DrawableLine(Color.Green, new Point(200, 30), new Point(200, 100)));       //lines
+        face.Add(new DrawableLine(Color.Blue, new Point(245, 30), new Point(245, 100)));        //lines
+        face.Add(new DrawableLine(Color.Indigo, new Point(273, 30), new Point(273, 100)));      //lines
+        face.Add(new DrawableLine(Color.Violet, new Point(295, 30), new Point(295, 100)));      //lines
+
+        int width = 400;
+        int length = 450;
+
+        Bitmap bitmap = new Bitmap(width, length);
+        Graphics graphic = Graphics.FromImage(bitmap);
+
+        foreach (var item in face)
+        {
+            item.Draw(graphic);
+            if (item is IWritable)
+            {
+                ((IWritable)item).Write(System.Console.Out);
+            }
+        }
+
+        graphic.Dispose();
+
+        bitmap.Save("face.png", ImageFormat.Png);
+        bitmap.Dispose();
+
+        Console.ReadKey();
+    }
+
+
+
     //***Interfaces***
     interface IDrawable
     {
@@ -77,11 +131,11 @@ namespace Assignment4_Comp123_Group001
         float start;
         //Methods
         DrawableArc(Color color, bool filled, Rectangle rectangle, float start, float end);
-        void Draw()
+        public void Draw(Graphics g)
         {
 
         }
-        void Write()
+        public void Write(TextWriter writer)
         {
 
         }
